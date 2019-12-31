255b06e13c9d52f1e6803d97024c8e0803583b8b
bobodalao <1171809512@qq.com>
2019-12-31 20:33:48 +0800

    first commit

---
+++ CMakeFiles/ICP.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bobo/Desktop/ICPCUDA-master/Cuda/containers/device_memory.cpp" "/home/bobo/Desktop/ICPCUDA-master/build/CMakeFiles/ICP.dir/Cuda/containers/device_memory.cpp.o"
+  "/home/bobo/Desktop/ICPCUDA-master/Cuda/containers/initialization.cpp" "/home/bobo/Desktop/ICPCUDA-master/build/CMakeFiles/ICP.dir/Cuda/containers/initialization.cpp.o"
+  "/home/bobo/Desktop/ICPCUDA-master/ICP.cpp" "/home/bobo/Desktop/ICPCUDA-master/build/CMakeFiles/ICP.dir/ICP.cpp.o"
+  "/home/bobo/Desktop/ICPCUDA-master/ICPOdometry.cpp" "/home/bobo/Desktop/ICPCUDA-master/build/CMakeFiles/ICP.dir/ICPOdometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda-9.0/include"
+  "../third-party/Sophus"
+  "../third-party/Pangolin/include"
+  "../third-party/Pangolin/build/src/include"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/ICP.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_compile_1.dir/Cuda/cuda_compile_1_generated_estimate.cu.o"
+  "CMakeFiles/cuda_compile_1.dir/Cuda/cuda_compile_1_generated_pyrdown.cu.o"
+  "CMakeFiles/ICP.dir/ICP.cpp.o"
+  "CMakeFiles/ICP.dir/ICPOdometry.cpp.o"
+  "CMakeFiles/ICP.dir/Cuda/containers/device_memory.cpp.o"
+  "CMakeFiles/ICP.dir/Cuda/containers/initialization.cpp.o"
+  "ICP.pdb"
+  "ICP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ICP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
